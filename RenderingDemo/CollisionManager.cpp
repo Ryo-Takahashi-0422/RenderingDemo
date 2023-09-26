8b8492505d8ce551778adfa1aac471658a5c64b6
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2023-09-26 22:09:55 +0900

    OBB collider and meshe are now correctly positioned and correctly drawn.

--- RenderingDemo/CollisionManager.cpp
+++ RenderingDemo/CollisionManager.cpp
@@ -8,34 +8,13 @@ CollisionManager::CollisionManager(ComPtr<ID3D12Device> _dev, std::vector<Resour
 	Init();
 }
 
-
-// TODO : 1. シェーダーを分けて、ボーンマトリックスとの乗算をなくす&エッジのみ着色したボックスとして表示する、2. 8頂点の位置を正す
+// TODO : 1. シェーダーを分けて、ボーンマトリックスとの乗算をなくす&エッジのみ着色したボックスとして表示する、2. 8頂点の位置を正す 3. 複数のメッシュ(障害物)とキャラクターメッシュを判別して処理出来るようにする
 void CollisionManager::Init()
 {
 	auto vetmap1 = resourceManager[0]->GetIndiceAndVertexInfo();
 	auto vetmap2 = resourceManager[1]->GetIndiceAndVertexInfo();
-
-	///
-	auto loalRotation = resourceManager[0]->GetLocalRotationFloat();
-	XMMATRIX localRotaionXMatrix = XMMatrixRotationX(loalRotation.x);
-	XMMATRIX localRotaionYMatrix = XMMatrixRotationY(45); // blenderと符号逆
-	XMMATRIX localRotaionZMatrix = XMMatrixRotationZ(loalRotation.z);
-	XMMATRIX localRotaionMatrix = /*localRotaionXMatrix * */localRotaionYMatrix/* * localRotaionZMatrix*/;
-	XMVECTOR quaternion = XMQuaternionRotationMatrix(localRotaionMatrix);
-	///
-	//resourceManager[0]->GetMappedMatrix()->world *= localRotaionMatrix; // モデルとコライダー描画はworld原点中心に回転するも、コライダー実態には影響無し。惜しい。これ+コライダーのworld原点回転か...?
-	
-	//XMVECTOR qu = XMQuaternionRotationAxis();
 	for (int j = 0; j < resourceManager[0]->GetVertexTotalNum(); ++j)
 	{
-		//// 回転している場合も考慮する...以下コードは平行移動にしかならない。
-		//XMVECTOR pos = XMLoadFloat3(&vetmap1.begin()->second.vertices[j].pos);
-		//pos.m128_f32[3] = 1;
-		//pos = XMVector3Rotate(pos, quaternion);//XMVector3Transform(pos, localRotaionMatrix);
-		//vetmap1.begin()->second.vertices[j].pos.x = pos.m128_f32[0];
-		//vetmap1.begin()->second.vertices[j].pos.y = pos.m128_f32[1];
-		//vetmap1.begin()->second.vertices[j].pos.z = pos.m128_f32[2];
-
 		// ★ オブジェクトが原点からオフセットしている場合、コライダーがx軸に対して対称の位置に配置される。これを調整してモデル描画の位置をコライダーと同位置に変えている。少しずれている？
 		vetmap1.begin()->second.vertices[j].pos.z *= -1;
 		input1.push_back(vetmap1.begin()->second.vertices[j].pos);
@@ -46,45 +25,42 @@ void CollisionManager::Init()
 		input2.push_back(vetmap2.begin()->second.vertices[j].pos);
 	}
 
-	/*BoundingBox*/BoundingOrientedBox::CreateFromPoints(box1, input1.size(), input1.data(), (size_t)sizeof(XMFLOAT3));
-	// fbxモデルデータからのxyz回転成分に基づきOBB回転
-	XMVECTOR scale;
-	XMVECTOR rotation;
-	XMVECTOR transition;
-	XMMatrixDecompose(&scale, &rotation, &transition, localRotaionMatrix);
-	/*box1.Transform(box1, 1.0f, rotation, transition);*/
-
-	/*resourceManager[0]->GetMappedMatrix()->world *= localRotaionMatrix;*/
-	///
-
+	BoundingOrientedBox::CreateFromPoints(box1, input1.size(), input1.data(), (size_t)sizeof(XMFLOAT3));
 	BoundingBox::CreateFromPoints(box2, 4454, input2.data(), (size_t)sizeof(XMFLOAT3));
 	BoundingSphere::CreateFromPoints(bSphere, 4454, input2.data(), (size_t)sizeof(XMFLOAT3));
 	bSphere.Radius -= 0.2f; // 球体コライダーの半径を微調整。数値適当
 
-
-
-	XMFLOAT3 temp1[8];
-	XMFLOAT3 temp2[8];
-	XMFLOAT3 temp3[8];
-	box1.GetCorners(temp1);
-	box2.GetCorners(temp2);
-	
-	//localRotaionMatrix.r[0].m128_f32[0] *= -1;
-	//localRotaionMatrix.r[0].m128_f32[2] *= -1;
-	//localRotaionMatrix.r[2].m128_f32[0] *= -1;
-	//localRotaionMatrix.r[2].m128_f32[2] *= -1;
-	//auto k = XMVector3Transform(XMLoadFloat3(&box1.Center), localRotaionMatrix);
-	//box1.Center.x = k.m128_f32[0];
-	//box1.Center.y = k.m128_f32[1];
-	//box1.Center.z = k.m128_f32[2];
-
+	box1.GetCorners(output1);
+	box2.GetCorners(output2);
+	// メッシュ描画に対してx軸対称の位置に配置されるコライダーを調整したが、元に戻して描画位置と同じ位置にコライダーを描画させる。(コライダー位置には影響無いことに注意)
 	for (int i = 0; i < 8; ++i)
 	{
-		output1[i] = temp1[i];
-		output1[i].z *= -1; // x軸に対して対称の位置に配置されるコライダーを調整したが、元に戻して描画位置と同じ位置にコライダーを描画させる。
-		output2[i] = temp2[i];
+		output1[i].z *= -1; 
 	}
 
+	// fbxモデルのxyzローカル回転値から回転行列を作成して、メッシュとコライダーの描画に利用するworld変換行列に乗算しておく。これで描画座標にローカル空間での回転情報が反映される。
+	auto localRotation = resourceManager[0]->GetLocalRotationFloat();
+	XMMATRIX localRotaionXMatrix = XMMatrixRotationX(localRotation.x);
+	XMMATRIX localRotaionYMatrix = XMMatrixRotationY(localRotation.y); // blenderと符号逆
+	XMMATRIX localRotaionZMatrix = XMMatrixRotationZ(localRotation.z);
+	XMMATRIX localRotaionMatrix = /*localRotaionXMatrix * */localRotaionYMatrix/* * localRotaionZMatrix*/;
+	XMVECTOR quaternion = XMQuaternionRotationMatrix(localRotaionMatrix);
+	resourceManager[0]->GetMappedMatrix()->world *= localRotaionMatrix; // モデルとコライダー描画をworld原点中心に回転させる。コライダーそのものは後に回転させる。
+
+	// コライダーの生成が完了し、コライダーの描画world変換行列にfbxローカル回転値を乗算してメッシュとコライダーの描画位置に反映させた状態。
+	// このままではコライダーの位置だけがfbxローカル回転値を反映していないので反映させる。コライダーのz座標はfbxモデルのそれに-1を乗算しており
+	// y軸回りの回転が逆転しているため、fbxローカル回転値に-1を乗算して同次座標系行列を作成して回転成分のXMVECTORを抽出して、コライダーの座標を
+	// ワールド空間原点に対して回転させて、メッシュとコライダー描画の位置に合わせている。(なおXMVecto3rRotateでメッシュ頂点そのものを回転させてもOBBではなくOBBだがAABBになる)
+	// ※この処理を無くしたいが、Blenderからのfbx export設定をいくら変えてもメッシュとコライダー座標のx軸反転問題が解決しない。
+	XMVECTOR scale; // ダミー
+	XMVECTOR rotation; // 回転成分のみ有効
+	XMVECTOR transition; //ダミー
+	localRotaionYMatrix = XMMatrixRotationY(-localRotation.y); // 符号逆転に注意
+	localRotaionMatrix = /*localRotaionXMatrix * */localRotaionYMatrix/* * localRotaionZMatrix*/;
+	XMMatrixDecompose(&scale, &rotation, &transition, localRotaionMatrix);
+	box1.Transform(box1, 1.0f, rotation, transition);
+
+	// 操作キャラクターの球体コライダー作成
 	CreateSpherePoints(bSphere.Center, bSphere.Radius);
 
 	auto heapProp = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
