9ce4925113bdda7c8a7b8fc3ee6abc3ae64d0d7c
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2023-11-03 13:14:21 +0900

    ほぼ修正無し

--- RenderingDemo/D3DX12Wrapper.cpp
+++ RenderingDemo/D3DX12Wrapper.cpp
@@ -351,13 +351,10 @@ bool D3DX12Wrapper::ResourceInit() {
 	
 
 	// 0 texture model
-	modelPath.push_back("C:\\Users\\RyoTaka\\Documents\\RenderingDemoRebuild\\FBX\\BattleField.txt");
-	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\BattleField_fixed.fbx");
-	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\BattleField_Test.fbx");
-	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\Boxs_diagonal.fbx"); 
-	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\Box_diagonal_lpos.fbx"); 
-	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\Box_diagonal.fbx"); 
+	//modelPath.push_back("C:\\Users\\RyoTaka\\Documents\\RenderingDemoRebuild\\FBX\\BattleField.txt");
+
 	// 3 texture model
+	modelPath.push_back("C:\\Users\\RyoTaka\\Documents\\RenderingDemoRebuild\\FBX\\Ziggrat.txt");
 	//modelPath.push_back("C:\\Users\\RyoTaka\\Desktop\\batllefield\\ancient\\ziggurat_test2.fbx");
 	 
 	// 4 textures model
