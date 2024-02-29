9d9dcdc872d9a1c9537c295aa0c907f855f0b373
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2024-02-29 16:15:00 +0900

    影の中でのキャラクターの法線描画を弱める処理を追加した

--- RenderingDemo/shader/FBXPixel.hlsl
+++ RenderingDemo/shader/FBXPixel.hlsl
@@ -4,11 +4,20 @@ float4 FBXPS(Output input) : SV_TARGET
 {
     float4 result;
     
-    //float3 light = normalize(float3(0, 1, 0));
-    //float3 lightColor = float3(1, 1, 1);
+    float4 shadowPos = mul(mul(proj, shadowView), input.worldPosition);
+    shadowPos.xyz /= shadowPos.w;
+    float2 shadowUV = 0.5 + float2(0.5, -0.5) * /*shadowPos.xy*/(input.lvPos.xy / input.lvPos.w);
+    float2 shadowValue = vsmmap.Sample(smp, shadowUV).xy;
+
+    float lz = input.lvDepth;
+    lz = length(input.worldPosition - input.light) / input.adjust;
     
-    //// ディフューズ計算
-    //float diffuseB = saturate(dot(light, input.norm.xyz));
+    if (input.isEnhanceShadow)
+    {
+        lz = input.trueDepth;
+        shadowValue = float2(vsmmap.Sample(smp, shadowUV).z, vsmmap.Sample(smp, shadowUV).z * vsmmap.Sample(smp, shadowUV).z);
+    }
+    
     float tangentWeight = 1.0f;
     unsigned int biNormalWeight = 0.3; // 0でUVシームが多少目立たなくなる
     float brightMin = 0.3f;
@@ -22,11 +31,9 @@ float4 FBXPS(Output input) : SV_TARGET
         brightEmpha = 1.3f;
         nor += 0.75f;
         brightMin = 0.2f;
-        
-        //input.normal.y *= -sign(charaRot[0].y) * charaRot[0].x /* * 0.5f*/;
-        //input.normal.z *= charaRot[0].x/* * 0.5f*/;
     }
     input.normal.x *= charaRot[0].z * sign(sunDIr.x); // 太陽のx座標符号によりセルフシャドウの向きを反転させる処理
+    
     // タイリング対応
     int uvX = abs(input.uv.x);
     int uvY = abs(input.uv.y);
@@ -34,61 +41,37 @@ float4 FBXPS(Output input) : SV_TARGET
     input.uv.y = abs(input.uv.y) - uvY;
         
     float3 normCol = normalmap.Sample(smp, input.uv);
-    //return float4(normCol, 1);
     float3 normVec = normCol * 2.0f - 1.0f;
     normVec = normalize(normVec);
-    //return float4(normVec, 1);
-    
     float3 normal = dot(sunDIr, normalize(-input.rotatedNorm.xyz)) + input.tangent * tangentWeight * normVec.x + input.biNormal * normVec.y * biNormalWeight + input.normal * normVec.z;
-    //return float4(normal, 1);
     normal *= -sunDIr.y;
-    //return float4(input.lightTangentDirection.xyz, 1);
+    
+    // 動き回るキャラクターについて、影の中では法線によるライティングを弱める。でないと影の中でもあたかも太陽光を受けているような見た目になる。
+    if (lz - 0.01f > shadowValue.x && input.isChara)
+    {
+        normal *= 0.2;
+    }
+
     float bright = dot(abs(input.lightTangentDirection.xyz), normal);
     bright = max(brightMin, bright);
     bright = saturate(bright * brightEmpha);
-    
-    float4 col = colormap.Sample(smp, input.uv);
-    if (col.a == 0) discard; // アルファ値が0なら透過させる
-    
-    //return col;
-    
+       
     float2 scrPos = input.screenPosition.xy / input.screenPosition.w; // 処理対象頂点のスクリーン上の座標。視錐台空間内の座標に対してwで除算して、スクリーンに投影するための立方体の領域（-1≦x≦1、-1≦y≦1そして0≦z≦1）に納める。
     scrPos = 0.5 + float2(0.5, -0.5) * scrPos;
     float airZ = distance(input.worldPosition, eyePos) / 300; // areialの奥行は300
     float4 air = airmap.Sample(smp, float3(scrPos, saturate(airZ)));
     float3 inScatter = air.xyz;
 
+    float4 col = colormap.Sample(smp, input.uv);
+    if (col.a == 0)
+        discard; // アルファ値が0なら透過させる
     result = float4(bright * col.x, bright * col.y, bright * col.z, 1);
-    //if(col.x !=0)
-    //{
     
-    
-    float4 shadowPos = mul(mul(proj, shadowView), input.worldPosition);
-    shadowPos.xyz /= shadowPos.w;
-    float2 shadowUV = 0.5 + float2(0.5, -0.5) * /*shadowPos.xy*/(input.lvPos.xy / input.lvPos.w);  
-    float2 shadowValue = vsmmap.Sample(smp, shadowUV).xy;
 
-    float4 oriCol = result;
-    //shadowPos.z /= 65;
-    float lz = input.lvDepth;
-    lz = length(input.worldPosition - input.light) / input.adjust;
     
     
 
-    if (input.isEnhanceShadow)
-    {
-        lz = input.trueDepth;
-        shadowValue = float2(vsmmap.Sample(smp, shadowUV).z, vsmmap.Sample(smp, shadowUV).z * vsmmap.Sample(smp, shadowUV).z);
-    }
-    
-    if (input.isChara)
-    {
-        //float a = dot(normalize(input.rotatedNorm.xyz), sunDIr);
-        //if (a < 0)
-        //    a -= a;
-        //float b = clamp(a, 0, 1);
-        //result.xyz += a;
-    }
+
 
     if (lz /*- 0.01f*/ > shadowValue.x/* && lz <= 1.0f*/)
     {
@@ -127,10 +110,5 @@ float4 FBXPS(Output input) : SV_TARGET
     
     result = result * shadowFactor + float4(inScatter, 0);
     
-    return result; /* + float4(diffuseB * diffuse.r, diffuseB * diffuse.g, diffuseB * diffuse.b, 1)*/;
-    //}
-    //else
-    //{
-    //    return float4(diffuseB * diffuse.r, diffuseB * diffuse.g, diffuseB * diffuse.b, 1);
-    //}
+    return result;;
 }
