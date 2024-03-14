0a9e9cd5e08da7ef62ab4f260edc2a436c694bcd
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2024-03-14 13:51:33 +0900

    不要コード削除

--- RenderingDemo/shader/PeraPixel.hlsl
+++ RenderingDemo/shader/PeraPixel.hlsl
@@ -11,25 +11,17 @@ float4 ps(Output input) : SV_TARGET
     float y = 0.0f; // focus distance
     float dp = depthMap.Sample(smp, input.uv);
     dp = pow(dp, 20.0); // Calculation between small values (y-dp) isn't effective. 'dp' should be emphasized enough before (y-dp).
-    //return float4(dp, dp, dp, 1);
     
     float depthDiff = abs(y - dp);
-    //depthDiff = pow(depthDiff, 20.0f);
+    depthDiff = pow(depthDiff, 2.0f);
     
-    float2 uvSize = float2(0.5f, 0.5f);
-    float2 uvOfst = float2(0, 0);
-
     float4 retColor[2];
     retColor[0] = colorTex.Sample(smp, input.uv);
-
     retColor[1] = bluredColor.Sample(smp, input.uv);;
     
     depthDiff = saturate(depthDiff * 2);
     ret = lerp(retColor[0], retColor[1], depthDiff);
     
-    
-    
-    //float4 color = colorTex.Sample(smp, input.uv);
     float4 imgui = imguiTex.Sample(smp, input.uv);
     float4 ssao = ssaoTex.Sample(smp, input.uv);
     
