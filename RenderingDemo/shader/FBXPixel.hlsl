f4ecb34c8750d5be3c824417117ebad73bc68e0a
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2024-04-07 17:16:05 +0900

    法線マップ強調処理を追加

--- RenderingDemo/shader/FBXPixel.hlsl
+++ RenderingDemo/shader/FBXPixel.hlsl
@@ -186,37 +186,6 @@ PixelOutput FBXPS(Output input) : SV_TARGET
         shadowValue = float2(vsmSample.z, vsmSample.z * vsmSample.z);
     }
     
-    //float tangentWeight = 1.0f;
-    //float biNormalWeight = 1.0f; // 0でUVシームが多少目立たなくなる
-    //float brightMin = 0.4f;
-    //float brightEmpha = 4.5f;
-    
-    //float Dot = dot(input.worldNormal, sunDIr);
-    //float nor = saturate(abs(Dot) + 0.5f);
-    
-    //float3 reflection = normalize(reflect(input.rotatedNorm.xyz, sunDIr));
-    //float3 speclur = dot(reflection, -input.ray);
-    //speclur = saturate(speclur);
-    //speclur *= speclurColor;
-    ////speclur = pow(speclur, 2);
-    //speclur *= -sunDIr.y;
-    
-    //input.normal.x *= charaRot[0].z * sign(sunDIr.x); // 太陽のx座標符号によりセルフシャドウの向きを反転させる処理
-    //if (input.isChara)
-    //{
-    //    //brightEmpha = 0.7f;
-    //    //nor += 2.1f;
-    //    //brightMin = 0.35f;
-    //    speclur = float3(0, 0, 0);
-    //    //tangentWeight = 1.0f;
-    //    //biNormalWeight = 0.3;
-    //    result.normal = float4(1, 1, 1, 1);
-    //    reflectedLight.directSpecular = 0.0f;
-    //}
-    //else
-    //{
-    //    result.normal = float4(input.worldNormal, 1);
-    //}
 
     // 大気のレンダリング
     float2 scrPos = input.screenPosition.xy / input.screenPosition.w; // 処理対象頂点のスクリーン上の座標。視錐台空間内の座標に対してwで除算して、スクリーンに投影するための立方体の領域（-1≦x≦1、-1≦y≦1そして0≦z≦1）に納める。
@@ -229,34 +198,12 @@ PixelOutput FBXPS(Output input) : SV_TARGET
     float3 normVec = normCol * 2.0f - 1.0f;
     normVec = normalize(normVec);
     
-    // 回転した法線のz前方は-方向(直したい...)なので、太陽方向もz成分をマイナス掛けする。この処理がないと太陽のx軸回転に対するキャラクターの陰が回転方向と反対側に出てしまう
-    //float3 rotatedNorm = normalize(input.rotatedNorm.xyz);
-    //float3 adjustDir = -sunDIr;
-    //adjustDir.z *= -1;
-    //float rotatedNormDot = dot(rotatedNorm, adjustDir);
-    
-    //float3 normal = /*rotatedNormDot + */input.tangent * tangentWeight * normVec.x + input.biNormal * normVec.y * biNormalWeight + input.normal * normVec.z;
-    //normal = normalize(normal);
-    //normal *= -sunDIr.y; // 太陽高度が低いほど目立たなくする
-    //// 動き回るキャラクターについて、影の中では法線によるライティングを弱める。でないと影の中でもあたかも太陽光を受けているような見た目になる。
-    //if (lz - 0.01f > shadowValue.x && input.isChara)
-    //{
-    //    normal *= 0.2;
-    //}
-
-    //float bright = dot(abs(input.lightTangentDirection.xyz), normal);
-    //bright = max(brightMin, bright);
-    //bright = saturate(bright * brightEmpha);
-    
-
-
-    //float4 col = colormap.Sample(smp, input.uv);
     float bright = 1.0f;
     result.col = float4(/*bright * */col.x, /*bright * */col.y, /*bright * */col.z, 1);
     
-    float3 lig = /*normalize(*/input.vLightDirection/*)*/;
+    float3 lig = normalize(input.vLightDirection);
     float diff/* = clamp(dot(normVec, lig), 0.1, 1.0)*/ = 0.0f;
-    float3 eye = /*normalize(*/input.vEyeDirection/*)*/;
+    float3 eye = normalize(input.vEyeDirection);
     float3 halfLE = normalize(lig + eye);
     //float spec = pow(clamp(dot(halfLE, normVec), 0.0, 1.0), 30.0f);
     float spec = dot(halfLE, normVec);
@@ -272,7 +219,7 @@ PixelOutput FBXPS(Output input) : SV_TARGET
     {
         //diff = clamp(dot(normVec, lig), 0.15f, 1.0f);
         //speclur *= 0.8f/*float3(0, 0, 0)*/;
-        result.normal = float4(input.worldNormal, 1);
+        result.normal = float4(1,1,1, 1);
         reflectedLight.directSpecular = 0.0f;
         spec4 *= 0.1f;
         bright = 2.3f;
@@ -289,35 +236,28 @@ PixelOutput FBXPS(Output input) : SV_TARGET
         }
         else
         {
-            diff = clamp(dot(normVec, lig), 0.15f, 1.0f);
+            //diff = clamp(dot(normVec, lig), 0.15f, 1.0f);
+            diff = clamp(dot(normVec, lig), 0.0f, 1.0f);
+            diff = pow(diff, 1.5f);
+            diff += 0.15f;
+            diff = saturate(diff);
+
         }
         
     }
     else
     {
-        diff = clamp(dot(normVec, lig), 0.1f, 1.0f);
+        diff = clamp(dot(normVec, lig), 0.0f, 1.0f);
+        diff = pow(diff, 2.0f); // 法線効果強調
+        diff += 0.1f;
+        diff = saturate(diff);
         result.normal = float4( /*float3(input.worldNormal + normVec)*/normVec, 1);
         spec4 *= 0.3f;
 
     }
     
-    //    // 動き回るキャラクターについて、影の中では法線によるライティングを弱める。でないと影の中でもあたかも太陽光を受けているような見た目になる。
-    //if (lz - 0.01f > shadowValue.x && input.isChara)
-    //{
-    //    diff *= 0.2;
-    //}
-    
     result.col *= diff;
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
+        
     // sponza壁のポール落ち影がキャラクターを貫通するのが目立つ問題への対策。キャラクターの法線と太陽ベクトルとの内積からキャラクター背面がポールからの落ち影を受けるかどうかを判定する。
     // シャドウマップがポールの値かどうかはvsmのアルファ値に格納したbooleanで判定している。
     //if (rotatedNormDot > 0)
@@ -373,6 +313,6 @@ PixelOutput FBXPS(Output input) : SV_TARGET
 
     result.col = result.col * shadowFactor + float4(inScatter, 0) * airDraw + /*float4(speclur, 0)*/spec4 + result.col * float4( /*reflectedLight.directDiffuse * 0.05f + */reflectedLight.directSpecular, 0) * -sunDIr.y;
     //result.col = spec4;
-    //result.col = float4( /*reflectedLight.directDiffuse * 0.05f + */reflectedLight.directSpecular, 0);
+    //result.col = float4( /*reflectedLight.directDiffuse * 0.05f + */reflectedLight.directSpecular, 0) * -sunDIr.y;
     return result;
 }
